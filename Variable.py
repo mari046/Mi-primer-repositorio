18bb1e2981b597a20fe49c23752ce064cd86a9ac
mari046 <zmariaangel65@gmail.com>
2026-04-26 17:56:52 -0500

    Create Variable.py for variable hand
    Add a Photoshop script for variable
    inicialization and user input

---
+++ Variable.py
@@ -0,0 +1,16 @@
+#Inicializando variables y colocandoles un valor
+nombre= "Mariaangel Zapata Fuentes" 
+edad= 14 
+Estado= True
+
+#Esta linea me imprime el contenido de una variable
+print("Bienvenido", mariaangel)
+print("Tu edad es", 14)
+
+#Uso de la funcion input()
+nombre=input("¿Cual es tu nombre? ")
+edad=input("¿Cual es tu edad? ")
+
+#Mostrar el resultado capturado con input()
+print("Bienvenido", mariaangel)
+print("Tu edad es", 14)
